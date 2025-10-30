be2cc3565e51fbcf52eb15f4337f3871cfa84f9c
crkteam <39040121+crkteam@users.noreply.github.com>
2025-10-30 15:18:33 +0800

    Update class-qrcode-extension.php

--- includes/class-qrcode-extension.php
+++ includes/class-qrcode-extension.php
@@ -243,7 +243,7 @@ class BookingPress_QRCode_Extension
      */
     private function generate_verification_code($booking_id)
     {
-        return 'BP' . str_pad($booking_id, 8, '0', STR_PAD_LEFT) . strtoupper(substr(md5(uniqid()), 0, 6));
+        return $booking_id . strtoupper(substr(md5(uniqid(mt_rand(), true)), 0, 4));
     }
 
     /**
